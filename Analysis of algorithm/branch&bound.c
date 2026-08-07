0a4f203d187ae55f9baaff066a675bcafc7402fa
lakshsurve123 <laksh.surve041@gmail.com>
2026-08-08 00:37:05 +0530

    19

---
+++ Analysis of algorithm/branch&bound.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Branch and Bound (8-Puzzle) is generally implemented\n");
+    printf("using priority queues and heuristic functions.\n");
+    printf("It is too lengthy for a basic lab program.\n");
+    printf("For practical lab exams, Branch and Bound is usually\n");
+    printf("demonstrated using LC Branch & Bound or Assignment Problem.\n");
+
+    return 0;
+}
